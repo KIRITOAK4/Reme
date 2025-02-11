b3f824a0362c0e1ec1035e3a5188161a71971abf
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2025-02-11 12:20:40 +0530

    Update function.py

--- helper/function.py
+++ helper/function.py
@@ -5,7 +5,7 @@ from Krito import Text, Text1, Text2, Text3, MAX_PAGE
 from pyrogram.types import InlineKeyboardButton
 
 # Setup logging
-logging.basicConfig(level=logging.INFO, filename="lameda_error.log")
+logging.basicConfig(level=logging.INFO, filename="function_error.log")
 logger = logging.getLogger("GifHandler")
 
 # Function to get a random GIF from the 'gif' directory
@@ -19,11 +19,11 @@ def get_page_gif():
         logger.error(f"Error in get_page_gif: {e}")
         return None
 
-# Function to format caption text based on the page number
+# Function to format caption text
 def get_page_caption(page_number, first_name, last_name, mention, username, id):
     try:
         text_templates = {1: Text, 2: Text1, 3: Text2, 4: Text3}
-        page_text = text_templates.get(page_number, Text)  # Default to Text if page not found
+        page_text = text_templates.get(page_number, Text)
         mention = f"[{first_name}](tg://user?id={id})"
         username_text = f"@{username}" if username else ""
         return page_text.format(
@@ -33,15 +33,16 @@ def get_page_caption(page_number, first_name, last_name, mention, username, id):
         logger.error(f"Error in get_page_caption: {e}")
         return None
 
-# Function to generate inline keyboard navigation buttons
+# Function to generate inline keyboard
 def get_inline_keyboard(page_number):
     try:
         buttons = []
         if page_number > 1:
-            buttons.append(InlineKeyboardButton("👈", callback_data=f"previous:{page_number-1}"))
+            buttons.append(InlineKeyboardButton("👈", callback_data="previous"))
         if page_number < MAX_PAGE:
-            buttons.append(InlineKeyboardButton("👉", callback_data=f"next:{page_number+1}"))
+            buttons.append(InlineKeyboardButton("👉", callback_data="next"))
         return [buttons] if buttons else []
     except Exception as e:
         logger.error(f"Error in get_inline_keyboard: {e}")
         return None
+        
