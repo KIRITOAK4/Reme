2c80addbbc5dce366853e6c2a744216f66093b60
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2025-07-13 12:04:54 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,12 +1,11 @@
-import asyncio
 import logging
 from time import time
 from uuid import uuid4
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, CallbackQuery
+from pyrogram import filters
 from pyrogram.enums import ParseMode
-from helper.database import db
+from pyrogram.types import InlineKeyboardMarkup
 from Krito import pbot, MAX_PAGE
+from helper.database import db
 from helper.function import get_page_gif, get_page_caption, get_inline_keyboard
 
 logging.basicConfig(level=logging.INFO, filename="start_callback_errors.log")
@@ -16,20 +15,21 @@ user_pages = {}
 
 @pbot.on_message(filters.private & filters.command("start"))
 async def start(client, message):
+    user_id = message.from_user.id
+
+    user_details = {
+        "id": user_id,
+        "first_name": message.from_user.first_name,
+        "last_name": message.from_user.last_name,
+        "username": message.from_user.username,
+        "mention": message.from_user.mention,
+    }
+
     try:
-        user_id = message.from_user.id
-
-        user_details = {
-            "id": user_id,
-            "first_name": message.from_user.first_name,
-            "last_name": message.from_user.last_name,
-            "username": message.from_user.username,
-            "mention": message.from_user.mention,
-        }
-
         if not await db.is_user_exist(user_id):
             await db.add_user(client, message)
 
+        # Token-based logic
         if len(message.command) > 1:
             input_token = message.command[1]
             stored_token, stored_time = await db.get_token_and_time(user_id)
@@ -47,51 +47,48 @@ async def start(client, message):
             await message.reply_text("Thanks for your support!")
             return
 
-        user_pages[user_id] = 1
+        # Initial start page
         page_number = 1
-
+        user_pages[user_id] = page_number
         caption = get_page_caption(page_number, **user_details)
-        inline_keyboard = get_inline_keyboard(page_number)
+        keyboard = InlineKeyboardMarkup(get_inline_keyboard(page_number))
 
         await message.reply_video(
             video=get_page_gif(),
             caption=caption,
             supports_streaming=True,
-            reply_markup=InlineKeyboardMarkup(inline_keyboard),
+            reply_markup=keyboard,
             parse_mode=ParseMode.MARKDOWN
         )
+
     except Exception as e:
-        logger.error(f"Error in start command: {e}")
+        logger.exception("Error in /start command")
 
 @pbot.on_callback_query(filters.regex(r"^(previous|next)$"))
-async def callback_query(client, callback_query):
+async def handle_navigation(client, cq):
+    user_id = cq.from_user.id
+    current_page = user_pages.get(user_id, 1)
+
     try:
-        user_id = callback_query.from_user.id
-        current_page = int(user_pages.get(user_id, 1))
+        if cq.data == "previous":
+            new_page = max(1, current_page - 1)
+        else:  # "next"
+            new_page = min(MAX_PAGE, current_page + 1)
 
-        if callback_query.data == "previous":
-            page_number = max(1, current_page - 1)
-        elif callback_query.data == "next":
-            page_number = min(MAX_PAGE, current_page + 1)
-        else:
-            return
-
-        user_pages[user_id] = page_number
+        user_pages[user_id] = new_page
 
         user_details = {
             "id": user_id,
-            "first_name": callback_query.from_user.first_name,
-            "last_name": callback_query.from_user.last_name,
-            "username": callback_query.from_user.username,
-            "mention": callback_query.from_user.mention,
+            "first_name": cq.from_user.first_name,
+            "last_name": cq.from_user.last_name,
+            "username": cq.from_user.username,
+            "mention": cq.from_user.mention,
         }
 
-        caption = get_page_caption(page_number, **user_details)
-        inline_keyboard = get_inline_keyboard(page_number)
+        caption = get_page_caption(new_page, **user_details)
+        keyboard = InlineKeyboardMarkup(get_inline_keyboard(new_page))
 
-        await callback_query.message.edit_caption(
-            caption=caption,
-            reply_markup=InlineKeyboardMarkup(inline_keyboard)
-        )
+        await cq.message.edit_caption(caption, reply_markup=keyboard)
+
     except Exception as e:
-        logger.error(f"Error in callback_query: {e}")
+        logger.exception("Error handling page navigation")
